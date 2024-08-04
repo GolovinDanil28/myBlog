c4bce33e024314a471ad47f2bae6b51dc140ec12
Golovin_DA <Danil.Golovin@advanta-group.ru>
2024-08-04 20:24:30 +0500

    add  send post email

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,19 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+from django.conf import settings
 
+class PostListView(ListView):
+        """
+        Альтернативное представление списка постов
+        """
+        queryset = Post.published.all()
+        context_object_name = 'posts'
+        paginate_by = 3
+        template_name = 'blog/post/list.html'
 def post_list(request):
     post_list = Post.published.all()
     # Постраничная разбивка с 3 постами на страницу
@@ -26,3 +37,25 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Этот код отправляет письмо на указанный адрес
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s ({cd['email']}) comments: {cd['comments']}"
+            send_mail(subject, message, settings.EMAIL_HOST_USER,
+                      [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
