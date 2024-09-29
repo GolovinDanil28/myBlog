bc43bc6c19b9736e2fff7c8ebe4c134c47f8206b
Golovin_DA <Danil.Golovin@advanta-group.ru>
2024-09-29 13:01:37 +0500

    add  class comment

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -8,6 +8,7 @@ class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'draft', 'Draft'
@@ -25,14 +26,36 @@ class Post(models.Model):
     objects = models.Manager()  # Менеджер по умолчанию
     published = PublishedManager()  # Менеджер для опубликованных постов
 
-    class Meta:
-        ordering = ['-publish']
-
-    def __str__(self):
-        return self.title
     def get_absolute_url(self):
         return reverse('blog:post_detail',
                        args=[self.publish.year,
                              self.publish.month,
                              self.publish.day,
                              self.slug])
+
+    class Meta:
+        ordering = ['-publish']
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
